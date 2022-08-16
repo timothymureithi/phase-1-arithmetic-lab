43dc5f3193826fa3c2dbb7e0f27fe0a5e2923f66
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-08-16 11:28:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let num3 = 28;
+let num4 = 6;
+
+let multiply = num1*num2;
+
+let random = (Math.floor(Math.random() + 1));
+
+let mod = (num3%num4);
+
+let max = (Math.max(1, 3, 5, 6, 12, 20));
